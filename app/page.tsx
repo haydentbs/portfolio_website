2d4fd5b17af2a310fb07f010cfd338235f9a95b3
Hayden Tibbals <hayden.tibbals@gmail.com>
2024-12-10 22:16:18 +0000

    buildable

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 import Footer from './components/Footer'
 import StarryBackground from './components/StarryBackground'
 
